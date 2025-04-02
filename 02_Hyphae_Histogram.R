eb74598f731e768bd51095a7e8df45a35422a3cb
tea9an <teagan.maloney@slu.edu>
2025-04-02 11:30:29 -0500

    Delete 02_Hyphae_Histogram.R
    extra copy

--- 02_Hyphae_Histogram.R
+++
@@ -1,9 +0,0 @@
-library(tidyverse)
-library(conflicted)
-conflict_prefer("filter", "dplyr")
-conflict_prefer("lag", "dplyr")
-library(ggplot2)
-
-X02_Dataset_Hyphae <- read_csv("Data/02_Dataset_Hyphae.csv")
-
-hist(X02_Dataset_Hyphae$Hyphae, main='Aseptate and Septate Hyphae', xlab="Total Hyphae")
